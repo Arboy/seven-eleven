d8336271ef821b55a43ca4dc294d1e1af700cb6f
Luke Reggie L. Balatayo <arboybalatayo@gmail.com>
2017-01-24 21:34:53 +0800

    update the @mock

--- src/main/java/com/eleven/seven/Helpers/Helpers.java
+++ src/main/java/com/eleven/seven/Helpers/Helpers.java
@@ -10,9 +10,9 @@ public class Helpers {
         return name + "123";
     }
 
-    public static int testFibonacci(int fib) {
+    public boolean testFibonacci(int fib) {
 
-        int n1 = 0, n2 = 1, n3, i, count = 18;
+        int n1 = 0, n2 = 1, n3, i, count =20    ;
 
 
         System.out.print("[" + n1 + "]" + " " + "[" + n2 + "]");//printing 0 and 1
@@ -23,11 +23,27 @@ public class Helpers {
             System.out.print(" " + "[" + n3 + "]");
             n1 = n2;
             n2 = n3;
-            if (n3 == fib) isFibonacci = true;
+            if (n3 == fib) {
+                isFibonacci = true;
+                break;
+            }
         }
 
         System.out.println(isFibonacci ? "\n\n" + fib + " is a Fibonacci " + isFibonacci + "" : "\n\n" + fib + " is not a Fibonacci " + isFibonacci);
 
-        return fib;
+        return isFibonacci;
+
+        /*int fibCount = 15;
+        int[] fib = new int[fibCount];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i < fibCount; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        for (int i = 0; i < fibCount; i++) {
+            System.out.print(fib[i] + " ");
+        }*/
     }
+
 }
--- src/test/java/com/eleven/seven/Helpers/HelpersTest.java
+++ src/test/java/com/eleven/seven/Helpers/HelpersTest.java
@@ -1,27 +1,75 @@
 package com.eleven.seven.Helpers;
 
+import javafx.beans.binding.When;
 import junit.framework.TestCase;
 import org.hibernate.boot.model.source.internal.hbm.Helper;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 /**
  * Created by JPMPC-B209 on 1/23/2017.
  */
-public class HelpersTest extends TestCase {
+public class HelpersTest /*extends TestCase */{
 
-    String testName = "Rboy123";
+    @Mock
+    Helpers helper;
+
+    int fibNumber = 8;
+    int [] sequence = {1,2,3,5,8,13};
+    boolean isSequence = true;
+
+    @Before
+    public  void create(){
+         initMocks(this);
+
+        when(helper.testFibonacci(13)).thenReturn(true);
+        //when(helper.add(1,3)).thenReturn(4);
+    }
+
     @Test
+    public void testFibonacci(){
+        //boolean fib = true;
+        //String fib = "[0] [1] [1] [2] [3] [5] [8] [13] [21] [34] [55] [89] [144] [233] [377] [610] [987] [1597] [2584] [4181]";
+
+        assertTrue(helper.testFibonacci(fibNumber));
+    }
+
+    @Test
+    public void fibSequence(){
+        int input = 13;
+        boolean fibNumber = helper.testFibonacci(input);
+
+        System.out.print("Actual sequence [ "+input+" ]");
+        System.out.print("[ "+ fibNumber +" ]");
+
+        System.out.print("\nExpected sequence [ "+input+" ]: ");
+        System.out.print("[ "+ isSequence +" ]");
+        for (int i=0; i<sequence.length; i ++) {
+            System.out.print("[" +sequence[i]+ "]");
+        }
+        assertTrue(helper.testFibonacci(13));
+    }
+
+
+    //String testName = "Rboy123";
+   /* @Test
     public void testThisMethod(){
         String name = "Rboy";
 
         assertEquals(testName, Helpers.testThisMethod(name));
-    }
+    }*/
 
-    int fib123 = 21;
+    /*int actual = 3;
     @Test
     public void testFibonacci(){
-        int fib = 21;
+        boolean fib = true;
+        //String fib = "[0] [1] [1] [2] [3] [5] [8] [13] [21] [34] [55] [89] [144] [233] [377] [610] [987] [1597] [2584] [4181]";
 
-        assertEquals(fib, Helpers.testFibonacci(fib123));
-    }
+        assertEquals(fib, Helpers.testFibonacci(actual));
+    }*/
 }
